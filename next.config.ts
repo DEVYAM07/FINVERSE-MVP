6f6a98c0b26734488f55d3df083fa4c05baf1e9d
DEVYAM MALHOTRA <devyammalhotradm07@gmail.com>
2025-06-28 12:54:59 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // ✅ Skip ESLint during `next build`
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
-
